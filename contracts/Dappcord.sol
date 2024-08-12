ec41a41ede18d81fb4cfb56af6911159e99cb713
trane7776 <izbasarov.t@edu.rea.ru>
2024-08-12 23:56:05 +0300

    added create channel func

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -4,9 +4,23 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Dappcord is ERC721URIStorage {
+  uint256 public totalChannels = 0;
+  address public owner;
+  struct Channel {
+    uint256 id;
+    string name;
+    uint256 cost;
+  }
+
+  mapping(uint256 => Channel) public channels;
   constructor(string memory _name, string memory _symbol)
     ERC721(_name, _symbol)
   {
-    
+    owner = msg.sender;
+  }
+
+  function createChannel(string memory _name, uint256 _cost) public {
+    totalChannels++;
+    channels[totalChannels] = Channel(totalChannels, _name, _cost);
   }
 }
--- test/Dappcord.js
+++ test/Dappcord.js
@@ -1,9 +1,47 @@
-const { expect } = require("chai")
-
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), 'ether');
+};
 
-describe("Dappcord", function () {
+describe('Dappcord', function () {
+  let dappcord;
+  let deployer, user;
+  const NAME = 'Dappcord';
+  const SYMBOL = 'DC';
 
-})
+  beforeEach(async () => {
+    [deployer, user] = await ethers.getSigners();
+    // Deploy Contract
+    const Dappcord = await ethers.getContractFactory('Dappcord');
+    dappcord = await Dappcord.connect(deployer).deploy(NAME, SYMBOL);
+
+    // Create a channel
+    const transaction = await dappcord
+      .connect(deployer)
+      .createChannel('general', tokens(1));
+    await transaction.wait();
+  });
+
+  describe('Deployment', () => {
+    it('Sets the name', async () => {
+      // Fetch name
+      let result = await dappcord.name();
+      // Check name
+      expect(result).to.equal(NAME);
+    });
+
+    it('Sets the symbol', async () => {
+      // Fetch symbol
+      let result = await dappcord.symbol();
+      // Check symbol
+      expect(result).to.equal(SYMBOL);
+    });
+
+    it('Sets the owner', async () => {
+      const result = await dappcord.owner();
+      expect(result).to.equal(deployer.address);
+    });
+  });
+  describe('Creating Channels', () => {});
+});
